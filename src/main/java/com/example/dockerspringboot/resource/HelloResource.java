0ed436f47462bfcb0d731e121eb4619fd6d47fbf
entinaia <36986732+entinaia@users.noreply.github.com>
2019-10-06 23:21:45 +0200

    Add files via upload

---
+++ src/main/java/com/example/dockerspringboot/resource/HelloResource.java
@@ -0,0 +1,46 @@
+/*
+ * Created on 07.09.2019
+ *
+ * Dimensions - Versionierung
+ * $Workfile: %PM% $
+ * $Revision: %PR% $
+ * $Date: %Date% $
+ * $Author: %Author% $
+ * 
+ * (c) Copyright SV Informatik GmbH 2010
+ */
+
+package com.example.dockerspringboot.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Zweck dieser Klasse: <br>
+ * (Hier Zweck eintragen - bis zu 2 Zeilen).
+ * <p>
+ * Angewandte Pattern: <br>
+ * (Hier Pattern nennen)
+ * <p>
+ * Zusammenhaenge / Partnerklassen <br>
+ * (Hier erlaeutern)
+ * <p>
+ * Abgrenzung zu anderen Klassen <br>
+ * (Hier erlaeutern)
+ * <p>
+ * Weitere Informationen <br>
+ * (Hier auffuehren)
+ * 
+ * @author N0008295
+ */
+@RestController
+@RequestMapping("rest/docker/hello")
+public class HelloResource {
+  
+  @GetMapping
+  public String hello() {
+    return "hello You";
+  }
+  
+}
